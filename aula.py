afc2a482df1cdf6adee62a0285f57f5768f4ba79
AlexandreMoresc <alexandrecoimbra987@gmail.com>
2026-04-11 18:15:39 -0300

    aprendendo a selecionar e preencher campos usando o selenium

--- aula.py
+++ aula.py
@@ -1,1 +1,19 @@
-print('hello')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class busca_cnpj():    
+
+    def iniciar(self):
+        self.driver = webdriver.Chrome()
+
+    def busca(self):
+        driver = self.driver
+        self.driver.get('https://google.com')
+        cnpj = driver.find_element(By.CLASS_NAME, 'gLFyf')
+        cnpj.send_keys('04.266.331/0019-58' + Keys.ENTER)
+
+if __name__ == '__main__':
+    busca = busca_cnpj()
+    busca.iniciar()
+    busca.busca()
